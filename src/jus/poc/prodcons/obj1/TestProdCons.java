a6ed34f605b6e1ca83f237f3e17702e8b7613592
Camboui <mathias.branger@gmail.com>
2015-12-12 19:48:23 +0100

    ajout parseur d'options

--- src/jus/poc/prodcons/obj1/TestProdCons.java
+++ src/jus/poc/prodcons/obj1/TestProdCons.java
@@ -8,6 +8,40 @@ import java.util.Properties;
 
 public class TestProdCons extends Simulateur {
 
+	int nbProd,nbCons,nbBuffer,nombreMoyenDeProduction;
+    int deviationNombreMoyenDeProduction,nombreMoyenDeConsommation;
+    int deviationNombreMoyenDeConsommation,tempsMoyenProduction;
+    int deviationTempsMoyenProduction,tempsMoyenConsommation;
+    int deviationTempsMoyenConsommation;
+    boolean inhiber;
+
+    protected int option;
+
+    protected void init(String file) {
+        Properties properties = new Properties();
+        
+        try {
+			properties.loadFromXML(ClassLoader.getSystemResourceAsStream(file));
+		} catch (InvalidPropertiesFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        nbProd= Integer.parseInt(properties.getProperty("nbProd"));
+        nbCons= Integer.parseInt(properties.getProperty("nbCons"));
+        nbBuffer= Integer.parseInt(properties.getProperty("nbBuffer"));
+        tempsMoyenProduction=Integer.parseInt(properties.getProperty("tempsMoyenProduction"));
+        deviationNombreMoyenDeProduction=Integer.parseInt(properties.getProperty("deviationNombreMoyenDeProduction"));
+        tempsMoyenConsommation=Integer.parseInt(properties.getProperty("tempsMoyenConsommation"));
+        deviationTempsMoyenConsommation=Integer.parseInt(properties.getProperty("deviationTempsMoyenConsommation"));
+        nombreMoyenDeProduction=Integer.parseInt(properties.getProperty("nombreMoyenDeProduction"));
+        deviationTempsMoyenProduction=Integer.parseInt(properties.getProperty("deviationTempsMoyenProduction"));
+        inhiber=Boolean.parseBoolean(properties.getProperty("inhiber"));
+    }
+    
 	public TestProdCons(Observateur observateur) {
 		super(observateur);
 		// TODO Auto-generated constructor stub
@@ -16,11 +50,13 @@ public class TestProdCons extends Simulateur {
 	@Override
 	protected void run() throws Exception {
 		// TODO Auto-generated method stub
-		
+		init("./jus/poc/prodcons/options/options.xml");
 	}
 	
 	public static void main(String[] args){
-		System.out.println("Bonjour");
+		System.out.println("exec");
+
+		new TestProdCons(new Observateur()).start();
 	}
    
 }
