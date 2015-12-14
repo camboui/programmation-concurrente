12b1405b612c9bf8e722ff6ee12668cfdf9ce661
P0ppoff <jules.hablot@gmail.com>
2015-12-15 00:02:23 +0100

    Add comment to ProcCons.java for David :)

--- src/jus/poc/prodcons/obj1/ProdCons.java
+++ src/jus/poc/prodcons/obj1/ProdCons.java
@@ -3,9 +3,10 @@ import jus.poc.prodcons.*;
 
 public class ProdCons implements Tampon {
 
-	int taille, nbPlein;
-	int in, out;
-	MessageX[] buffer;
+	int taille; // taille du buffer
+	int nbPlein; // combien de cases sont pleines
+	int in, out; // ou doit être mis/lu le prochain message
+	MessageX[] buffer; // le buffer en lui même
 	
 	public ProdCons(int taille){
 		this.nbPlein = 0;
@@ -26,26 +27,25 @@ public class ProdCons implements Tampon {
 	@Override
 	public synchronized MessageX get(_Consommateur arg0) throws Exception, InterruptedException {
 		while(nbPlein <= 0){
-			wait();
+			wait(); // on attend le prochain message s'il n'y a plus dans le buffer
 		} 
-		MessageX r = buffer[out];
-		out = (out+1)%taille;
-		nbPlein--;
-		notifyAll();
-		return  r;
+		MessageX r = buffer[out]; // on recupère le bon message
+		out = (out+1)%taille; // ou calcule ou sera le prochain message à lire
+		nbPlein--; // on elève une case du nombre de cases pleines
+		notifyAll(); // on reveille tout le monde histoire de voir si des producteurs ne pourraient pas mettre de nouveaux messages
+		return  r; // on retourne le message
 	}
 	
 
 	@Override
 	public synchronized void put(_Producteur arg0, Message m) throws Exception,InterruptedException {
 		while(nbPlein >= taille){
-			wait();
+			wait(); // on attent tant que le buffer est plein
 		}
-		buffer[in] = (MessageX) m;
-		in = (in+1)%taille;
-		nbPlein++;
-		notifyAll();
-		
+		buffer[in] = (MessageX) m; // on charge le message dans le buffer
+		in = (in+1)%taille; // on calcul la prochaine position du prochain message
+		nbPlein++; // on dit qu'un message de plus est à lire
+		notifyAll(); // on reveille tous les autres, histoire que quelqu'un puisse lire ce message ou un autre, ou dépose un nouveau message après celui là
 	}
 
 	@Override
