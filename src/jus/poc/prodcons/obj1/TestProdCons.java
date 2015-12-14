1fdbc2f2c924e19bfeb12ea29f78007a67848ea3
P0ppoff <jules.hablot@gmail.com>
2015-12-14 10:59:12 +0100

    Add start to all prod&cons

--- src/jus/poc/prodcons/obj1/TestProdCons.java
+++ src/jus/poc/prodcons/obj1/TestProdCons.java
@@ -65,13 +65,16 @@ public class TestProdCons extends Simulateur {
 		{
 			lesCons.add(new Consommateur(observateur, tempsMoyenConsommation, deviationTempsMoyenConsommation, data,toConsume.next()));
 		}
-		System.out.println();
 		//les faire communiquer
+		for(int i=0 ; i<lesProds.size() ; i++){
+			lesProds.get(i).start();
+		}
+		for(int i=0 ; i<lesCons.size() ; i++){
+			lesCons.get(i).start();
+		}
 	}
 	
 	public static void main(String[] args){
-		System.out.println("exec");
-
 		new TestProdCons(new Observateur()).start();
 	}
    
