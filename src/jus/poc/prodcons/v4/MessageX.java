be87a434af12a87a169e74f5b41878e33227e81b
Camboui <mathias.branger@gmail.com>
2015-12-15 20:53:08 +0100

    ajout classe sémaphore + préparation V4

---
+++ src/jus/poc/prodcons/v4/MessageX.java
@@ -0,0 +1,19 @@
+package jus.poc.prodcons.v4;
+import jus.poc.prodcons.*;
+
+ 
+public class MessageX implements Message {
+
+	int idProd; // Permet de stocker qui à produit ce message
+	int messNum;
+	
+	public MessageX(int idProd,int messNum ){
+		this.idProd = idProd;
+		this.messNum = messNum;
+	}
+	
+	public String toString(){
+		return "produit " + idProd + " message " + messNum;
+	}
+	
+}
---
+++ src/jus/poc/prodcons/v4/Semaphore.java
@@ -0,0 +1,27 @@
+package jus.poc.prodcons.v4;
+import jus.poc.prodcons.*;
+
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class Semaphore {
+    int residu;
+
+    public Semaphore(int residu) {
+        this.residu = residu;
+    }
+    
+    public synchronized void V() {
+        if(++residu<=0) {
+            notify();
+        }
+    }
+    
+    public synchronized void P() throws InterruptedException {
+        if(--residu<0){
+            wait();
+        }
+    }
+}
