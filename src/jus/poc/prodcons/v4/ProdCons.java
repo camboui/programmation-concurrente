21659a447ece2fc37d153aff3003f928f4c692aa
P0ppoff <jules.hablot@gmail.com>
2015-12-16 22:23:32 +0100

    meilleure compréhension du problème : lecteur de case vide du buffer

--- src/jus/poc/prodcons/v4/ProdCons.java
+++ src/jus/poc/prodcons/v4/ProdCons.java
@@ -34,15 +34,8 @@ public class ProdCons implements Tampon {
 		for(int i=(out%taille) ; i<(in%taille + taille) ; i++){
 			nbAttente += buffer[i%taille].nbMessage;
 		}
-		System.out.println("en attente() : "+nbAttente);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//System.out.println("en attente() : "+nbAttente);
 		return nbAttente;
-		//return (in-out)%taille;
 	}
 
 	@Override
@@ -52,7 +45,7 @@ public class ProdCons implements Tampon {
 		// Si quelqu'un accède déjà à la SC, celui qui arrive ici est mis en pause.
 		sMutex.P(); //On protège les données partagées
 		//this.enAttente();
-		System.out.println("Consommation");
+		//System.out.println("Consommation");
 		MessageX r = buffer[out]; // on recupère le bon message
 		if(!inhiber){
 			System.out.println("Consommateur " +  arg0.identification() + " : "+ r.toString());
@@ -60,9 +53,12 @@ public class ProdCons implements Tampon {
 		observateur.retraitMessage(arg0, r);
 		if(r.nbMessage == 1){
 			System.out.println("fin des exemplaires");
+			r.nbMessage--;
 			out = (out+1)%taille; // ou calcule ou sera le prochain message à lire
 			nbPlein--; // on elève une case du nombre de cases pleines
 			sExemplaires.V();
+		}else if(r.nbMessage == 0){
+			System.out.println("Erreur message vide");
 		}else{
 			System.out.println("il y a "+r.nbMessage+" il reste maintenant +"+(r.nbMessage-1));
 			r.nbMessage--;
@@ -78,6 +74,7 @@ public class ProdCons implements Tampon {
 	public void put(_Producteur arg0, Message m) throws Exception,InterruptedException {
 		sProd.P(); // On produit.Si tous les messages sont produits, on attend
 		// Si quelqu'un accède déjà à la SC, celui qui arrive ici est mis en pause.
+		// c'est pas plutôt que si on a plus de producteurs que de place, on ferme les nouveaux producteurs ???
 		sMutex.P(); //On protège les données partagées 
 		buffer[in] = (MessageX) m; // on charge le message dans le buffer
 		if(!inhiber){
@@ -87,8 +84,7 @@ public class ProdCons implements Tampon {
 		nbPlein++; // on dit qu'un message de plus est à lire
 		sMutex.V();// On redonne l'accès aux données partagées
 		sCons.V();//On reveille un consommateur
-		sExemplaires.P(); // bloquage du producteur
-		sProd.V();
+		//sExemplaires.P(); // bloquage du producteur
 		}
 
 	@Override
